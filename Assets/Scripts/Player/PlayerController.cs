a7dd3af58916005a0b2b0b2b2847c22c859a6b86
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-10-13 23:33:46 -0500

    setup camera movement based on if the mouse is at the edge of the screen

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,20 @@ namespace MOBA
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private Camera playerCamera;
-        [SerializeField] private float moveSpeed = 1;
+        [SerializeField] private float playerMovedSpeed = 1;
         [SerializeField] private float rotateSpeed = 1;
+        [SerializeField] private float cameraMoveSpeed = 1;
         [SerializeField] private LayerMask floorLayer;
 
         private int mouseRightClickIndex = 1;
         private Vector3 movePosition = new();
-        private Quaternion rotation = Quaternion.identity;
+        private Quaternion rotation = new();
 
         private Ray ray;
         private RaycastHit hit;
+
+        private Vector3 mousePosition = new();
+        private float deltaTime;
 
         private void OnEnable()
         {
@@ -26,7 +30,11 @@ namespace MOBA
             if (Input.GetMouseButtonDown(mouseRightClickIndex))
                 UpdatePlayerMovePosition();
 
+            mousePosition = Input.mousePosition;
+            deltaTime = Time.deltaTime;
+
             MovePlayer();
+            MoveCamera();
         }
 
         private void UpdatePlayerMovePosition()
@@ -39,14 +47,32 @@ namespace MOBA
 
         private void MovePlayer()
         {
-            transform.position = Vector3.MoveTowards(transform.position, movePosition, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, movePosition, playerMovedSpeed * deltaTime);
 
             //rotate if the player hasn't reached the move position yet
             if (transform.position != movePosition)
             {
                 rotation = Quaternion.LookRotation(movePosition - transform.position);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * deltaTime);
             }
+        }
+
+        /// <summary>
+        /// Move camera whenever the users mouse is at the edge of the screens.
+        /// </summary>
+        private void MoveCamera()
+        {
+            if (mousePosition.x <= 0)
+                playerCamera.transform.position -= Vector3.right * deltaTime * cameraMoveSpeed;
+
+            else if (mousePosition.x >= Screen.width)
+                playerCamera.transform.position += Vector3.right * deltaTime * cameraMoveSpeed;
+
+            if (mousePosition.y <= 0)
+                playerCamera.transform.position -= Vector3.forward * deltaTime * cameraMoveSpeed;
+
+            else if (mousePosition.y >= Screen.height)
+                playerCamera.transform.position += Vector3.forward * deltaTime * cameraMoveSpeed;
         }
     }
 }
