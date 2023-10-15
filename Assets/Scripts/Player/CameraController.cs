856a05e777b596904285c27f812c16c9ddf93fae
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-10-15 11:51:53 -0500

    separated camera controls and player controls into 2 separate scripts and generally cleaned them up

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+namespace MOBA
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private PlayerController playerController;
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private float cameraMoveSpeed = 1;
+
+        [Tooltip("The x offset applied to the cameras position whenever its snapped to the user.")]
+        [SerializeField] private float cameraSnapXOffset = 0;
+
+        [Tooltip("The y offset applied to the cameras position whenever its snapped to the user.")]
+        [SerializeField] private float cameraSnapYOffset = 0;
+
+        [Tooltip("Toggle camera controls on or off.")]
+        public bool cameraControlsActive = true;
+
+        /// <summary>
+        /// Returns true if the camera is snapped to the player this frame.
+        /// </summary>
+        public bool CameraSnappedToPlayer { get => cameraSnappedToPlayer; }
+        private bool cameraSnappedToPlayer = false;
+
+        private Vector3 verticalPoint = new();
+        private Vector3 horizontalPoint = new();
+        private Vector3 mousePosition = new();
+        private float deltaTime;
+
+        private void Update()
+        {
+            mousePosition = Input.mousePosition;
+            deltaTime = Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.Space))
+                SnapCameraToPlayer();
+            else
+                cameraSnappedToPlayer = false;
+
+            MoveCamera();
+        }
+
+        /// <summary>
+        /// Move camera whenever the users mouse is at the edges of the screen.
+        /// </summary>
+        private void MoveCamera()
+        {
+            if (cameraSnappedToPlayer || !cameraControlsActive)
+                return;
+
+            if (mousePosition.x <= 0)
+                playerCamera.transform.position -= Vector3.right * deltaTime * cameraMoveSpeed;
+
+            else if (mousePosition.x >= Screen.width)
+                playerCamera.transform.position += Vector3.right * deltaTime * cameraMoveSpeed;
+
+            if (mousePosition.y <= 0)
+                playerCamera.transform.position -= Vector3.forward * deltaTime * cameraMoveSpeed;
+
+            else if (mousePosition.y >= Screen.height)
+                playerCamera.transform.position += Vector3.forward * deltaTime * cameraMoveSpeed;
+        }
+
+        /// <summary>
+        /// Locks the camera to the player.
+        /// </summary>
+        private void SnapCameraToPlayer()
+        {
+            if (!cameraControlsActive)
+                return;
+
+            cameraSnappedToPlayer = true;
+
+            verticalPoint = Utilities.FindNearestPointOnLine(playerController.transform.position, Vector3.forward, playerCamera.transform.position);
+            horizontalPoint = Utilities.FindNearestPointOnLine(playerController.transform.position, Vector3.right, playerCamera.transform.position);
+
+            playerCamera.transform.position = new Vector3(
+                verticalPoint.x + cameraSnapXOffset,
+                playerCamera.transform.position.y,
+                horizontalPoint.z + cameraSnapYOffset);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,52 +5,20 @@ namespace MOBA
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private Camera playerCamera;
-        [SerializeField] private float playerMovedSpeed = 1;
-        [SerializeField] private float rotateSpeed = 1;
-        [SerializeField] private float cameraMoveSpeed = 1;
         [SerializeField] private LayerMask floorLayer;
-        [SerializeField] private float cameraCenterXOffset = 0;
-        [SerializeField] private float cameraCenterYOffset = 0;
 
         private int mouseRightClickIndex = 1;
-        //private Vector3 movePosition = new();
-        //private Quaternion rotation = new();
 
         private Ray ray;
         private RaycastHit hit;
 
-        private Vector3 mousePosition = new();
-        private float deltaTime;
-
-        private Vector3 verticalPoint = new();
-        private Vector3 horizontalPoint = new();
-
-        private bool cameraLockedOnPlayer = false;
-
-        public bool cameraControlsActive = true;
-
-        //private void OnEnable()
-        //{
-        //    movePosition = transform.position;
-        //}
-
+        [Tooltip("Represents the target transform the ai pathfinding is using.")]
         public Transform moveTarget;
 
         private void Update()
         {
             if (Input.GetMouseButtonDown(mouseRightClickIndex))
                 UpdatePlayerMovePosition();
-
-            if (Input.GetKey(KeyCode.Space))
-                LockCameraOnPlayer();
-            else
-                cameraLockedOnPlayer = false;
-
-            mousePosition = Input.mousePosition;
-            deltaTime = Time.deltaTime;
-
-            //MovePlayer();
-            MoveCamera();
         }
 
         private void UpdatePlayerMovePosition()
@@ -59,55 +27,6 @@ namespace MOBA
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, floorLayer))
                 moveTarget.position = hit.point;
-        }
-
-        //private void MovePlayer()
-        //{
-            //transform.position = Vector3.MoveTowards(transform.position, movePosition, playerMovedSpeed * deltaTime);
-
-            ////rotate if the player hasn't reached the move position yet
-            //if (transform.position != movePosition)
-            //{
-            //    rotation = Quaternion.LookRotation(movePosition - transform.position);
-            //    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * deltaTime);
-            //}
-        //}
-
-        /// <summary>
-        /// Move camera whenever the users mouse is at the edge of the screens.
-        /// </summary>
-        private void MoveCamera()
-        {
-            if (cameraLockedOnPlayer || !cameraControlsActive)
-                return;
-
-            if (mousePosition.x <= 0)
-                playerCamera.transform.position -= Vector3.right * deltaTime * cameraMoveSpeed;
-
-            else if (mousePosition.x >= Screen.width)
-                playerCamera.transform.position += Vector3.right * deltaTime * cameraMoveSpeed;
-
-            if (mousePosition.y <= 0)
-                playerCamera.transform.position -= Vector3.forward * deltaTime * cameraMoveSpeed;
-
-            else if (mousePosition.y >= Screen.height)
-                playerCamera.transform.position += Vector3.forward * deltaTime * cameraMoveSpeed;
-        }
-
-        private void LockCameraOnPlayer()
-        {
-            if (!cameraControlsActive)
-                return;
-
-            cameraLockedOnPlayer = true;
-
-            verticalPoint = Utilities.FindNearestPointOnLine(transform.position, Vector3.forward, playerCamera.transform.position);
-            horizontalPoint = Utilities.FindNearestPointOnLine(transform.position, Vector3.right, playerCamera.transform.position);
-
-            playerCamera.transform.position = new Vector3(
-                verticalPoint.x + cameraCenterXOffset, 
-                playerCamera.transform.position.y, 
-                horizontalPoint.z + cameraCenterYOffset);
         }
     }
 }
