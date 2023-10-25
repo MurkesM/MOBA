e371d887b1bef36c9963c96409e43d3bf724c528
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-10-24 19:06:03 -0500

    added some util functions

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -12,5 +12,35 @@ namespace MOBA
             float dotP = Vector3.Dot(directionOfPoint, lineDirection);
             return lineOrigin + lineDirection * dotP;
         }
+
+        /// <summary>
+        /// Returns the mouse position in screen space or world space. 
+        /// You'll need to set the z position.
+        /// Defaults to screen space.
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="inWorldSpace"></param>
+        /// <returns></returns>
+        public static Vector3 GetMousePosition(Camera camera, bool inWorldSpace = false)
+        {
+            return  inWorldSpace ? camera.ScreenToWorldPoint(Input.mousePosition) : Input.mousePosition;
+        }
+
+        /// <summary>
+        /// Performs a physics raycast, returning true if the raycast was successful.
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="hit"></param>
+        /// <param name="layerMask"></param>
+        /// <returns></returns>
+        public static bool GetMousePosition(Camera camera, out RaycastHit hit, int layerMask)
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+                return true;
+
+            return false;
+        }
     }
 }
